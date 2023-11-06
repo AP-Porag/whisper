bf9270d9990a326bfebf9662e07453c873c951c0
AP-Porag <smauhp@gmail.com>
2023-11-06 23:00:38 +0600

    Initial commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-white">
+    <Header/>
+    <HeroSlider/>
+  </div>
+</template>
+
+<script>
+import Header from "./partials/Header.vue";
+import HeroSlider from "./partials/HeroSlider.vue";
+
+export default {
+  name: "Hero",
+  components: {HeroSlider, Header},
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
